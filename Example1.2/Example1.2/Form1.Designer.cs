12e6df5a8914262be41aa4cdcf36f1f0470e5c25
KyryloMuzychka <kyryll2016@gmail.com>
2023-09-12 19:55:21 +0300

    Example1.2

---
+++ Example1.2/Example1.2/Form1.Designer.cs
@@ -0,0 +1,414 @@
+﻿namespace Example1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupFound = new GroupBox();
+            resultTextForFound = new TextBox();
+            resultForFound = new Label();
+            numberOfObjectButtonForFound = new Button();
+            ParseButtonForFound = new Button();
+            nonVirtualMethodButtonForFound = new Button();
+            jobButtonForFound = new Button();
+            toStringButtonForFound = new Button();
+            virtualMethodButtonForFound = new Button();
+            createObjectButton = new Button();
+            creditEditForFound = new TextBox();
+            nameEditForFound = new TextBox();
+            creditLabelForFound = new Label();
+            nameLabelForFound = new Label();
+            derivedGroup = new GroupBox();
+            debitName = new Label();
+            debitEdit = new TextBox();
+            resultTextForDerived = new TextBox();
+            resultForDerived = new Label();
+            numberOfObjectButtonForDerived = new Button();
+            parseButtonForDerived = new Button();
+            nonVirtualMethodButtonForDerived = new Button();
+            jobButtonForDerived = new Button();
+            toStringButtonForDerived = new Button();
+            virtualMethodButtonForDerived = new Button();
+            createObjectButtonForDerived = new Button();
+            creditEditForDerived = new TextBox();
+            nameEditForDerived = new TextBox();
+            creditLabelForDerived = new Label();
+            nameLabelForDerived = new Label();
+            groupFound.SuspendLayout();
+            derivedGroup.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupFound
+            // 
+            groupFound.Controls.Add(resultTextForFound);
+            groupFound.Controls.Add(resultForFound);
+            groupFound.Controls.Add(numberOfObjectButtonForFound);
+            groupFound.Controls.Add(ParseButtonForFound);
+            groupFound.Controls.Add(nonVirtualMethodButtonForFound);
+            groupFound.Controls.Add(jobButtonForFound);
+            groupFound.Controls.Add(toStringButtonForFound);
+            groupFound.Controls.Add(virtualMethodButtonForFound);
+            groupFound.Controls.Add(createObjectButton);
+            groupFound.Controls.Add(creditEditForFound);
+            groupFound.Controls.Add(nameEditForFound);
+            groupFound.Controls.Add(creditLabelForFound);
+            groupFound.Controls.Add(nameLabelForFound);
+            groupFound.Location = new Point(63, 90);
+            groupFound.Name = "groupFound";
+            groupFound.Size = new Size(632, 925);
+            groupFound.TabIndex = 0;
+            groupFound.TabStop = false;
+            groupFound.Text = "Found";
+            // 
+            // resultTextForFound
+            // 
+            resultTextForFound.Location = new Point(21, 737);
+            resultTextForFound.Multiline = true;
+            resultTextForFound.Name = "resultTextForFound";
+            resultTextForFound.Size = new Size(561, 164);
+            resultTextForFound.TabIndex = 12;
+            // 
+            // resultForFound
+            // 
+            resultForFound.AutoSize = true;
+            resultForFound.Location = new Point(253, 655);
+            resultForFound.Name = "resultForFound";
+            resultForFound.Size = new Size(78, 32);
+            resultForFound.TabIndex = 11;
+            resultForFound.Text = "Result";
+            // 
+            // numberOfObjectButtonForFound
+            // 
+            numberOfObjectButtonForFound.Location = new Point(329, 560);
+            numberOfObjectButtonForFound.Name = "numberOfObjectButtonForFound";
+            numberOfObjectButtonForFound.Size = new Size(253, 46);
+            numberOfObjectButtonForFound.TabIndex = 10;
+            numberOfObjectButtonForFound.Text = "Number Of Object";
+            numberOfObjectButtonForFound.UseVisualStyleBackColor = true;
+            numberOfObjectButtonForFound.Click += numberOfObjectButtonForFound_Click;
+            // 
+            // ParseButtonForFound
+            // 
+            ParseButtonForFound.Location = new Point(329, 482);
+            ParseButtonForFound.Name = "ParseButtonForFound";
+            ParseButtonForFound.Size = new Size(253, 46);
+            ParseButtonForFound.TabIndex = 9;
+            ParseButtonForFound.Text = "Parse";
+            ParseButtonForFound.UseVisualStyleBackColor = true;
+            ParseButtonForFound.Click += ParseButtonForFound_Click;
+            // 
+            // nonVirtualMethodButtonForFound
+            // 
+            nonVirtualMethodButtonForFound.Location = new Point(329, 406);
+            nonVirtualMethodButtonForFound.Name = "nonVirtualMethodButtonForFound";
+            nonVirtualMethodButtonForFound.Size = new Size(253, 46);
+            nonVirtualMethodButtonForFound.TabIndex = 8;
+            nonVirtualMethodButtonForFound.Text = "Non Virtual Method";
+            nonVirtualMethodButtonForFound.UseVisualStyleBackColor = true;
+            nonVirtualMethodButtonForFound.Click += nonVirtualMethodButtonForFound_Click;
+            // 
+            // jobButtonForFound
+            // 
+            jobButtonForFound.Location = new Point(21, 560);
+            jobButtonForFound.Name = "jobButtonForFound";
+            jobButtonForFound.Size = new Size(235, 46);
+            jobButtonForFound.TabIndex = 7;
+            jobButtonForFound.Text = "Job";
+            jobButtonForFound.UseVisualStyleBackColor = true;
+            jobButtonForFound.Click += jobButtonForFound_Click;
+            // 
+            // toStringButtonForFound
+            // 
+            toStringButtonForFound.Location = new Point(21, 482);
+            toStringButtonForFound.Name = "toStringButtonForFound";
+            toStringButtonForFound.Size = new Size(235, 46);
+            toStringButtonForFound.TabIndex = 6;
+            toStringButtonForFound.Text = "To String";
+            toStringButtonForFound.UseVisualStyleBackColor = true;
+            toStringButtonForFound.Click += toStringButtonForFound_Click;
+            // 
+            // virtualMethodButtonForFound
+            // 
+            virtualMethodButtonForFound.Location = new Point(21, 406);
+            virtualMethodButtonForFound.Name = "virtualMethodButtonForFound";
+            virtualMethodButtonForFound.Size = new Size(235, 46);
+            virtualMethodButtonForFound.TabIndex = 5;
+            virtualMethodButtonForFound.Text = "Virtual Method";
+            virtualMethodButtonForFound.UseVisualStyleBackColor = true;
+            virtualMethodButtonForFound.Click += virtualMethodButtonForFound_Click;
+            // 
+            // createObjectButton
+            // 
+            createObjectButton.Location = new Point(171, 267);
+            createObjectButton.Name = "createObjectButton";
+            createObjectButton.Size = new Size(235, 46);
+            createObjectButton.TabIndex = 4;
+            createObjectButton.Text = "Create Object";
+            createObjectButton.UseVisualStyleBackColor = true;
+            createObjectButton.Click += createObjectButton_Click;
+            // 
+            // creditEditForFound
+            // 
+            creditEditForFound.Location = new Point(131, 154);
+            creditEditForFound.Name = "creditEditForFound";
+            creditEditForFound.Size = new Size(451, 39);
+            creditEditForFound.TabIndex = 3;
+            // 
+            // nameEditForFound
+            // 
+            nameEditForFound.Location = new Point(131, 85);
+            nameEditForFound.Name = "nameEditForFound";
+            nameEditForFound.Size = new Size(451, 39);
+            nameEditForFound.TabIndex = 2;
+            // 
+            // creditLabelForFound
+            // 
+            creditLabelForFound.AutoSize = true;
+            creditLabelForFound.Location = new Point(21, 161);
+            creditLabelForFound.Name = "creditLabelForFound";
+            creditLabelForFound.Size = new Size(78, 32);
+            creditLabelForFound.TabIndex = 1;
+            creditLabelForFound.Text = "Credit";
+            // 
+            // nameLabelForFound
+            // 
+            nameLabelForFound.AutoSize = true;
+            nameLabelForFound.Location = new Point(21, 92);
+            nameLabelForFound.Name = "nameLabelForFound";
+            nameLabelForFound.Size = new Size(78, 32);
+            nameLabelForFound.TabIndex = 0;
+            nameLabelForFound.Text = "Name";
+            // 
+            // derivedGroup
+            // 
+            derivedGroup.Controls.Add(debitName);
+            derivedGroup.Controls.Add(debitEdit);
+            derivedGroup.Controls.Add(resultTextForDerived);
+            derivedGroup.Controls.Add(resultForDerived);
+            derivedGroup.Controls.Add(numberOfObjectButtonForDerived);
+            derivedGroup.Controls.Add(parseButtonForDerived);
+            derivedGroup.Controls.Add(nonVirtualMethodButtonForDerived);
+            derivedGroup.Controls.Add(jobButtonForDerived);
+            derivedGroup.Controls.Add(toStringButtonForDerived);
+            derivedGroup.Controls.Add(virtualMethodButtonForDerived);
+            derivedGroup.Controls.Add(createObjectButtonForDerived);
+            derivedGroup.Controls.Add(creditEditForDerived);
+            derivedGroup.Controls.Add(nameEditForDerived);
+            derivedGroup.Controls.Add(creditLabelForDerived);
+            derivedGroup.Controls.Add(nameLabelForDerived);
+            derivedGroup.Location = new Point(749, 90);
+            derivedGroup.Name = "derivedGroup";
+            derivedGroup.Size = new Size(632, 925);
+            derivedGroup.TabIndex = 13;
+            derivedGroup.TabStop = false;
+            derivedGroup.Text = "Derived";
+            // 
+            // debitName
+            // 
+            debitName.AutoSize = true;
+            debitName.Location = new Point(21, 220);
+            debitName.Name = "debitName";
+            debitName.Size = new Size(72, 32);
+            debitName.TabIndex = 14;
+            debitName.Text = "Debit";
+            // 
+            // debitEdit
+            // 
+            debitEdit.Location = new Point(131, 213);
+            debitEdit.Name = "debitEdit";
+            debitEdit.Size = new Size(451, 39);
+            debitEdit.TabIndex = 13;
+            // 
+            // resultTextForDerived
+            // 
+            resultTextForDerived.Location = new Point(21, 737);
+            resultTextForDerived.Multiline = true;
+            resultTextForDerived.Name = "resultTextForDerived";
+            resultTextForDerived.Size = new Size(561, 164);
+            resultTextForDerived.TabIndex = 12;
+            // 
+            // resultForDerived
+            // 
+            resultForDerived.AutoSize = true;
+            resultForDerived.Location = new Point(253, 655);
+            resultForDerived.Name = "resultForDerived";
+            resultForDerived.Size = new Size(78, 32);
+            resultForDerived.TabIndex = 11;
+            resultForDerived.Text = "Result";
+            // 
+            // numberOfObjectButtonForDerived
+            // 
+            numberOfObjectButtonForDerived.Location = new Point(329, 560);
+            numberOfObjectButtonForDerived.Name = "numberOfObjectButtonForDerived";
+            numberOfObjectButtonForDerived.Size = new Size(253, 46);
+            numberOfObjectButtonForDerived.TabIndex = 10;
+            numberOfObjectButtonForDerived.Text = "Number Of Object";
+            numberOfObjectButtonForDerived.UseVisualStyleBackColor = true;
+            numberOfObjectButtonForDerived.Click += numberOfObjectButtonForDerived_Click;
+            // 
+            // parseButtonForDerived
+            // 
+            parseButtonForDerived.Location = new Point(329, 482);
+            parseButtonForDerived.Name = "parseButtonForDerived";
+            parseButtonForDerived.Size = new Size(253, 46);
+            parseButtonForDerived.TabIndex = 9;
+            parseButtonForDerived.Text = "Parse";
+            parseButtonForDerived.UseVisualStyleBackColor = true;
+            parseButtonForDerived.Click += parseButtonForDerived_Click;
+            // 
+            // nonVirtualMethodButtonForDerived
+            // 
+            nonVirtualMethodButtonForDerived.Location = new Point(329, 406);
+            nonVirtualMethodButtonForDerived.Name = "nonVirtualMethodButtonForDerived";
+            nonVirtualMethodButtonForDerived.Size = new Size(253, 46);
+            nonVirtualMethodButtonForDerived.TabIndex = 8;
+            nonVirtualMethodButtonForDerived.Text = "Non Virtual Method";
+            nonVirtualMethodButtonForDerived.UseVisualStyleBackColor = true;
+            nonVirtualMethodButtonForDerived.Click += nonVirtualMethodButtonForDerived_Click;
+            // 
+            // jobButtonForDerived
+            // 
+            jobButtonForDerived.Location = new Point(21, 560);
+            jobButtonForDerived.Name = "jobButtonForDerived";
+            jobButtonForDerived.Size = new Size(235, 46);
+            jobButtonForDerived.TabIndex = 7;
+            jobButtonForDerived.Text = "Job";
+            jobButtonForDerived.UseVisualStyleBackColor = true;
+            jobButtonForDerived.Click += jobButtonForDerived_Click;
+            // 
+            // toStringButtonForDerived
+            // 
+            toStringButtonForDerived.Location = new Point(21, 482);
+            toStringButtonForDerived.Name = "toStringButtonForDerived";
+            toStringButtonForDerived.Size = new Size(235, 46);
+            toStringButtonForDerived.TabIndex = 6;
+            toStringButtonForDerived.Text = "To String";
+            toStringButtonForDerived.UseVisualStyleBackColor = true;
+            toStringButtonForDerived.Click += toStringButtonForDerived_Click;
+            // 
+            // virtualMethodButtonForDerived
+            // 
+            virtualMethodButtonForDerived.Location = new Point(21, 406);
+            virtualMethodButtonForDerived.Name = "virtualMethodButtonForDerived";
+            virtualMethodButtonForDerived.Size = new Size(235, 46);
+            virtualMethodButtonForDerived.TabIndex = 5;
+            virtualMethodButtonForDerived.Text = "Virtual Method";
+            virtualMethodButtonForDerived.UseVisualStyleBackColor = true;
+            virtualMethodButtonForDerived.Click += virtualMethodButtonForDerived_Click;
+            // 
+            // createObjectButtonForDerived
+            // 
+            createObjectButtonForDerived.Location = new Point(171, 267);
+            createObjectButtonForDerived.Name = "createObjectButtonForDerived";
+            createObjectButtonForDerived.Size = new Size(235, 46);
+            createObjectButtonForDerived.TabIndex = 4;
+            createObjectButtonForDerived.Text = "Create Object";
+            createObjectButtonForDerived.UseVisualStyleBackColor = true;
+            createObjectButtonForDerived.Click += createObjectButtonForDerived_Click;
+            // 
+            // creditEditForDerived
+            // 
+            creditEditForDerived.Location = new Point(131, 154);
+            creditEditForDerived.Name = "creditEditForDerived";
+            creditEditForDerived.Size = new Size(451, 39);
+            creditEditForDerived.TabIndex = 3;
+            // 
+            // nameEditForDerived
+            // 
+            nameEditForDerived.Location = new Point(131, 85);
+            nameEditForDerived.Name = "nameEditForDerived";
+            nameEditForDerived.Size = new Size(451, 39);
+            nameEditForDerived.TabIndex = 2;
+            // 
+            // creditLabelForDerived
+            // 
+            creditLabelForDerived.AutoSize = true;
+            creditLabelForDerived.Location = new Point(21, 161);
+            creditLabelForDerived.Name = "creditLabelForDerived";
+            creditLabelForDerived.Size = new Size(78, 32);
+            creditLabelForDerived.TabIndex = 1;
+            creditLabelForDerived.Text = "Credit";
+            // 
+            // nameLabelForDerived
+            // 
+            nameLabelForDerived.AutoSize = true;
+            nameLabelForDerived.Location = new Point(21, 92);
+            nameLabelForDerived.Name = "nameLabelForDerived";
+            nameLabelForDerived.Size = new Size(78, 32);
+            nameLabelForDerived.TabIndex = 0;
+            nameLabelForDerived.Text = "Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1466, 1060);
+            Controls.Add(derivedGroup);
+            Controls.Add(groupFound);
+            Name = "Form1";
+            Text = "Form1";
+            groupFound.ResumeLayout(false);
+            groupFound.PerformLayout();
+            derivedGroup.ResumeLayout(false);
+            derivedGroup.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupFound;
+        private Button createObjectButton;
+        private TextBox creditEditForFound;
+        private TextBox nameEditForFound;
+        private Label creditLabelForFound;
+        private Label nameLabelForFound;
+        private Button nonVirtualMethodButtonForFound;
+        private Button jobButtonForFound;
+        private Button toStringButtonForFound;
+        private Button virtualMethodButtonForFound;
+        private TextBox resultTextForFound;
+        private Label resultForFound;
+        private Button numberOfObjectButtonForFound;
+        private Button ParseButtonForFound;
+        private GroupBox derivedGroup;
+        private TextBox resultTextForDerived;
+        private Label resultForDerived;
+        private Button numberOfObjectButtonForDerived;
+        private Button parseButtonForDerived;
+        private Button nonVirtualMethodButtonForDerived;
+        private Button jobButtonForDerived;
+        private Button toStringButtonForDerived;
+        private Button virtualMethodButtonForDerived;
+        private Button createObjectButtonForDerived;
+        private TextBox creditEditForDerived;
+        private TextBox nameEditForDerived;
+        private Label creditLabelForDerived;
+        private Label nameLabelForDerived;
+        private Label debitName;
+        private TextBox debitEdit;
+    }
+}
---
+++ Example1.2/Example1.2/Form1.cs
@@ -0,0 +1,219 @@
+﻿namespace Example1._2
+{
+    public partial class Form1 : Form
+    {
+
+        /// <summary>
+        /// Родительский класс
+        /// </summary>
+        protected class Found
+        {
+            //fields
+            protected string name;
+            protected int credit;
+            static protected int count;
+            const string NL = "\r\n";
+            //Constructors
+            public Found()
+            {
+                name = "Nemo";
+                credit = 0;
+                count++;
+            }
+            public Found(string name, int credit)
+            {
+                this.name = name;
+                this.credit = credit;
+                count++;
+            }
+            public override string ToString()
+            {
+                string s = "Поля: name = {0}, credit = {1}";
+                return String.Format(s, name, credit);
+            }
+            public string NonVirtualMethod()
+            {
+                return "Found: " + this.ToString();
+            }
+            public virtual string VirtualMethod()
+            {
+                return "Found: " + this.ToString();
+            }
+            public string Parse()
+            {
+                return "Выполнен разбор кода!";
+            }
+            public string Job()
+            {
+                string res = "";
+                res += "VirtMethod: " + VirtualMethod() + NL;
+                res += "NonVirtMethod: " + NonVirtualMethod() + NL;
+                res += "Parse: " + Parse() + NL;
+                return res;
+            }
+            public static string NumberOfObjects()
+            {
+                return "Объектов создано: " + count;
+            }
+
+        }
+
+
+        protected class Derived : Found
+        {
+            //добавление и скрытие полей
+            protected double debit;
+            new protected double credit;
+            //Конструкторы
+            public Derived() : base()
+            {
+                debit = 0; credit = 0;
+            }
+            public Derived(string name, double debit,
+                double credit) : base(name, (int)credit)
+            {
+                this.debit = debit;
+                this.credit = credit;
+            }
+            //Методы
+            public string MyBaseCredit()
+            {
+                return base.credit.ToString();
+            }
+            new public string Parse()
+            {
+                string res = base.Parse();
+                res += "Выполнена проверка кода!";
+                return res;
+            }
+            public override string ToString()
+            {
+                string s = "Поля: name = {0}, Basecredit = {1}" + "credit = {2}, debit = {3}";
+                return String.Format(s, name, base.credit, credit, debit);
+            }
+            public override string VirtualMethod()
+            {
+                return "Derived: " + this.ToString();
+            }
+        }
+
+
+        Found found;
+        Derived derived;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            found = new Found();
+            derived = new Derived();
+        }
+
+        private void createObjectButton_Click(object sender, EventArgs e)
+        {
+            string name = nameEditForFound.Text;
+            int credit = 0;
+            try
+            {
+                credit = int.Parse(creditEditForFound.Text);
+                if (name != "")
+                    found = new Found(name, credit);
+                resultTextForFound.Text = "Объект класса Found успешно создан!";
+            }
+            catch (Exception)
+            {
+                resultTextForFound.Text = "Некорректно заданы числовые данные!";
+            }
+        }
+
+        private void virtualMethodButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = found.VirtualMethod();
+            resultTextForFound.Text = res;
+        }
+
+        private void nonVirtualMethodButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = found.NonVirtualMethod();
+            resultTextForFound.Text = res;
+        }
+
+        private void toStringButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = found.ToString();
+            resultTextForFound.Text = res;
+        }
+
+        private void ParseButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = found.Parse();
+            resultTextForFound.Text = res;
+        }
+
+        private void jobButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = found.Job();
+            resultTextForFound.Text = res;
+        }
+
+        private void numberOfObjectButtonForFound_Click(object sender, EventArgs e)
+        {
+            string res = Found.NumberOfObjects();
+            resultTextForFound.Text = res;
+        }
+
+        private void createObjectButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string name = nameEditForDerived.Text;
+            double credit = 0, debit = 0;
+            try
+            {
+                credit = double.Parse(creditEditForDerived.Text);
+                debit = double.Parse(debitEdit.Text);
+            }
+            catch (Exception)
+            {
+                debitEdit.Text = "Некорректно заданы числовые данные!";
+            }
+            if (name != "")
+                derived = new Derived(name, debit, credit);
+            resultTextForDerived.Text = "Объект класса Derived успешно создан!";
+        }
+
+        private void virtualMethodButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = derived.VirtualMethod();
+            resultTextForDerived.Text = res;
+        }
+
+        private void nonVirtualMethodButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = derived.NonVirtualMethod();
+            resultTextForDerived.Text = res;
+        }
+
+        private void toStringButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = derived.ToString();
+            resultTextForDerived.Text = res;
+        }
+
+        private void parseButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = derived.Parse();
+            resultTextForDerived.Text = res;
+        }
+
+        private void jobButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = derived.Job();
+            resultTextForDerived.Text = res;
+        }
+
+        private void numberOfObjectButtonForDerived_Click(object sender, EventArgs e)
+        {
+            string res = Derived.NumberOfObjects();
+            resultTextForDerived.Text = res;
+        }
+    }
+}
